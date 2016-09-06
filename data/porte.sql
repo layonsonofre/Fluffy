059fe9fd9411e2026de7d3cfdcd51ee166af87d9
Andre Garcia <andrecadgarcia@gmail.com>
2016-09-05 21:20:54 -0300

    Adds porte, servico, servico_tem_data tables' dump

---
+++ data/porte.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM pet_shop.porte
+-- Date: 2016-09-05 21:03
+*/
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (1,'Mini',1,28,0,6);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (2,'Mini Peludo',1,28,0,6);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (3,'Pequeno',29,35,7,15);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (4,'Médio 1',36,49,16,25);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (5,'Médio 2',36,49,16,25);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (6,'Grande 1',50,69,26,45);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (7,'Grande 2',50,69,26,45);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (8,'Extra',70,100,46,60);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (9,'Pelo Curto',1,100,0,100);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (10,'Pelo Longo',1,100,0,100);
+INSERT INTO `porte` (`id`,`nome`,`tamanho_minimo`,`tamanho_maximo`,`peso_minimo`,`peso_maximo`) VALUES (11,'Todos',1,100,0,100);
