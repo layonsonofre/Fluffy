bf0204950c7c38134b6599c417e9cf5f6bf6ce1a
Andre Garcia <andrecadgarcia@gmail.com>
2016-09-08 01:35:33 -0300

    Adds generic http model get

--- angular_flask/servico.py
+++ angular_flask/servico.py
@@ -5,33 +5,20 @@ from flask import send_file, make_response, abort
 from angular_flask import app
 from datetime import datetime
 
-from fluffyModel import FluffyModel
-from util import *
-
-class Servico(FluffyModel):
+class Servico():
 	def __init__(self, data):
-		FluffyModel.__init__(self, ["id","nome","porte","preco"], data)
 		self.id = data[0]
 		self.nome = data[1]
 		self.porte = data[2]
 		self.preco = data[3]
 
+	def toJSON(self):
+		return {
+			"id":		self.id,
+			"nome":		self.nome,
+			"porte":	self.porte,
+			"preco":	self.preco
+		}
+
 	def fromJSON():
 		print("")
-
-@app.route('/servico/', methods=['GET'])
-def get_servico(servico_id=None, porte_id=None):
-
-	servico_id = request.args.get('servico')
-	porte_id = request.args.get('porte')
-
-	data = Util.getData('getServico', [servico_id, porte_id])
-	list = []
-	if len(data) == 0 :
-		return jsonify(success=True,result=list,message="Nenhum servico cadastrado")
-	for info in data:
-		list.append(Servico(info))
-	if len(data) == 1 :
-		return jsonify(success=True,result=list[0].toJSON(),message="")
-	else :
-		return jsonify(success=True,result=[s.toJSON() for s in list],message="")
